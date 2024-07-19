02e779291683b4190c1553568a887c08ff9461da
Burak Açıker <acikerburak@gmail.com>
2024-07-19 14:52:28 +0300

    initial commit

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Burak on 19.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func kisiEkleButton(_ sender: UIButton) {
+    }
+    
+}
+
